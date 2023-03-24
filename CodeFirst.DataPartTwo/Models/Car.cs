44a6cc9c5cde68ab41d096a709462b491d32920d
Ognjen Stankovic <ognjenogistankovic2002@gmail.com>
2023-03-24 14:33:16 +0100

    Added models for Car database

---
+++ CodeFirst.DataPartTwo/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.DataPartTwo.Models
+{
+	public class Car
+	{
+        //color, year, chassis number, brand, model
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string ChassisNumber { get; set; } 
+        public Engine Engine { get; set; }
+        public int EngineId { get; set; }
+
+    }
+}
---
+++ CodeFirst.DataPartTwo/Models/EngineType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.DataPartTwo.Models
+{
+	public class EngineType
+	{
+		public int Id { get; set; }
+		public string Model { get; set; }
+		public string Name { get; set; }
+		public List<Engine> Engines { get; set; }
+	}
+}
