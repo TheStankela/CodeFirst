9574b005ceff7e4dc44c3c7738f0be1a45be3252
Ognjen Stankovic <ognjenogistankovic2002@gmail.com>
2023-03-24 14:33:25 +0100

    added db context for cars

---
+++ CodeFirst.DataPartTwo/CarContext.cs
@@ -0,0 +1,21 @@
+﻿using CodeFirst.DataPartTwo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.DataPartTwo
+{
+	public class CarContext : DbContext
+	{
+		public DbSet<Car> Cars { get; set; }
+		public DbSet<Engine> Engines { get; set; }
+		public DbSet<EngineType> EngineTypes { get; set; }
+		public CarContext(DbContextOptions options) : base(options)
+		{
+
+		}
+	}
+}
