03074cae85e5300d3eaaf131974f545a550ca4d4
Ognjen Stankovic <ognjenogistankovic2002@gmail.com>
2023-03-24 15:19:37 +0100

    Added carRepository

---
+++ CodeFirst.DataPartTwo/Repository/CarRepository.cs
@@ -0,0 +1,26 @@
+﻿using CodeFirst.DataPartTwo.Interface;
+using CodeFirst.DataPartTwo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.DataPartTwo.Repository
+{
+    public class CarRepository : ICarRepository
+	{
+		public CarContext _carContext;
+		public CarRepository(CarContext carContext)
+		{
+			_carContext = carContext;
+		}
+
+		public List<Car> GetAllCars()
+		{
+			return _carContext.Cars.Include(c => c.Engine).ToList();
+		}
+
+	}
+}
