0526a0870f411baf5d5d21ebe1e42e9888503bb0
Ognjen Stankovic <ognjenogistankovic2002@gmail.com>
2023-03-27 19:54:02 +0200

    TASK 1 - Added unit tests project

---
+++ CodeFirst.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CodeFirst.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
